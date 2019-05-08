636800a0eeb4393927bee6f984a2b9106d58294d
pc <jesus.cortez.1991@gmail.com>
2019-05-08 09:50:06 -0400

    agregando mapa

--- lib/pages/home/TaskHomeMap.dart
+++ lib/pages/home/TaskHomeMap.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,7 +10,18 @@ class taskHomeMap extends StatefulWidget {
 
 class _MytaskPageMapState extends State<taskHomeMap> {
 
-  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Future initState() {
@@ -23,7 +36,18 @@ class _MytaskPageMapState extends State<taskHomeMap> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
