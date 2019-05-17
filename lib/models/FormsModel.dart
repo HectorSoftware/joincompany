fed10ad0576e20f6b402c3ea34270e862c69d2aa
Stalin Sánchez <cejonio@gmail.com>
2019-05-17 15:10:07 -0400

    Modelo de Forms

---
+++ lib/models/FormsModel.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:joincompany/models/FormModel.dart';
+
+class Forms {
+  int currentPage;
+  List<Form> data;
+  String firstPageUrl;
+  int from;
+  int lastPage;
+  String lastPageUrl;
+  String nextPageUrl;
+  String path;
+  int perPage;
+  String prevPageUrl;
+  int to;
+  int total;
+
+  Forms({
+    this.currentPage,
+    this.data,
+    this.firstPageUrl,
+    this.from,
+    this.lastPage,
+    this.lastPageUrl,
+    this.nextPageUrl,
+    this.path,
+    this.perPage,
+    this.prevPageUrl,
+    this.to,
+    this.total,
+  });
+
+  factory Forms.fromJson(String str) => Forms.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Forms.fromMap(Map<String, dynamic> json) => new Forms(
+    currentPage: json["current_page"],
+    data: json["data"] != null ? new List<Form>.from(json["data"].map((x) => Form.fromMap(x))) : null,
+    firstPageUrl: json["first_page_url"],
+    from: json["from"],
+    lastPage: json["last_page"],
+    lastPageUrl: json["last_page_url"],
+    nextPageUrl: json["next_page_url"],
+    path: json["path"],
+    perPage: json["per_page"],
+    prevPageUrl: json["prev_page_url"],
+    to: json["to"],
+    total: json["total"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "current_page": currentPage,
+    "data": data != null ? new List<Form>.from(data.map((x) => x.toMap())) : null,
+    "first_page_url": firstPageUrl,
+    "from": from,
+    "last_page": lastPage,
+    "last_page_url": lastPageUrl,
+    "next_page_url": nextPageUrl,
+    "path": path,
+    "per_page": perPage,
+    "prev_page_url": prevPageUrl,
+    "to": to,
+    "total": total,
+  };
+}
