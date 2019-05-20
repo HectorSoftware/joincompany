f57a672de97129211cb938990246f828d1697530
SanvicenteCesar <cesarsvp17@gmail.com>
2019-05-20 15:56:43 -0400

    Merge branch 'JesusHoy' of https://github.com/HectorSoftware/joincompany into CesarSanvicenteNuevo
    # Conflicts:
    #	lib/pages/LoginPage.dart

--- lib/pages/home/TaskHomeTask.dart
+++ lib/pages/home/TaskHomeTask.dart
@@ -147,7 +147,10 @@ class _MytaskPageTaskState extends State<taskHomeTask> {
                                         children: <Widget>[
                                           Text(title, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
                                           Text(address, style: TextStyle(fontSize: 10)),
-                                          IconButton(icon: Icon(Icons.delete), onPressed: (){}),
+                                          Padding(
+                                            padding: const EdgeInsets.only(right: 200),
+                                            child: IconButton(icon: Icon(Icons.delete), onPressed: (){}),
+                                          ),
                                         ],
                                       ),
                                     ),
@@ -182,20 +185,23 @@ class _MytaskPageTaskState extends State<taskHomeTask> {
     );
   }
 
+  Map<String, String> intsToMonths = {
+    '1':  'Enero',
+    '2':  'Febrero',
+    '3':  'Marzo',
+    '4':  'Abril',
+    '5':  'Mayo',
+    '6':  'Junio',
+    '7':  'Julio',
+    '8':  'Agosto',
+    '9':  'Septiembre',
+    '10': 'Octubre',
+    '11': 'Noviembre',
+    '12': 'Diciembre',
+  };
+
   String obtenerMes(String mes){
-    if(mes == '1'){return 'Enero';}
-    if(mes == '2'){return 'Febrero';}
-    if(mes == '3'){return 'Marzo';}
-    if(mes == '4'){return 'Abril';}
-    if(mes == '5'){return 'Mayo';}
-    if(mes == '6'){return 'Junio';}
-    if(mes == '7'){return 'Julio';}
-    if(mes == '8'){return 'Agosto';}
-    if(mes == '9'){return 'Septiembre';}
-    if(mes == '10'){return 'Octubre';}
-    if(mes == '11'){return 'Noviembre';}
-    if(mes == '12'){return 'Diciembre';}
-    return 'Hoy';
+    return intsToMonths[mes];
   }
 }
 
