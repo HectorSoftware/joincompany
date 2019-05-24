75e9233fcde9362b1a31c9e10a98f8fdd8af3c27
SanvicenteCesar <cesarsvp17@gmail.com>
2019-05-24 09:37:15 -0400

    Merge branches 'CesarSanvicenteNuevo' and 'luis_view' of https://github.com/HectorSoftware/joincompany into CesarSanvicenteNuevo
    # Conflicts:
    #	lib/blocs/blocTaskForm.dart
    #	lib/models/WidgetsList.dart

--- lib/blocs/blocTaskForm.dart
+++ lib/blocs/blocTaskForm.dart
@@ -12,7 +12,6 @@ class BlocTaskForm  {
 
   ListWidgets items = new ListWidgets();
   List<Widget> listWidget = List<Widget>();
-  List<FieldModel> listFieldPrint = List<FieldModel>();
   String token;
   String customer;
   var idFormType;
@@ -30,40 +29,119 @@ class BlocTaskForm  {
   Sink<String> get inTaskType => _taskTypeFormController.sink;
 
 
-  final  saveFormController   = StreamController<String>();
-  Stream<String> get outSaveForm => saveFormController.stream;
-  Sink<String> get inSaveForm => saveFormController.sink;
+  final  _saveFormController   = StreamController<String>();
+  Stream<String> get outSaveForm => _saveFormController.stream;
+  Sink<String> get inSaveForm => _saveFormController.sink;
 
   final  _formController   = StreamController<String>();
   Stream<String> get outForm => _formController.stream;
   Sink<String> get inForm => _formController.sink;
 
-  final  fieldModelController   = StreamController<List<FieldModel>>();
-  Stream<List<FieldModel>> get outFieldModel=> fieldModelController.stream;
-  Sink<List<FieldModel>> get inFieldModel=> fieldModelController.sink;
-
-
-  void updateListWidget(BuildContext context) {
-
-    try {
-      if (idFormType != null) {
-        for (SectionModel v in form.sections) {
-          for (FieldModel k in v.fields) {
-            listFieldPrint.add(k);
-
+  void updateListWidget(context){
+    listWidget.clear();
+    if(idFormType != null)
+    {
+      for(SectionModel v in form.sections)
+      {
+        for(FieldModel k in v.fields)
+        {
+          camposWidgets = k;
+          switch(k.fieldType){
+            case 'Combo':
+              {
+                optionsElements = k.fieldOptions;
+                listWidget.add(items.createState().tab(optionsElements,context));
+              }
+              break;
+            case 'Text':
+              {
+                final nameController = TextEditingController();
+                listWidget.add(items.createState().text(context,k.name,nameController ));
+              }
+              break;
+            case 'Textarea':
+              {
+                final nameController = TextEditingController();
+                listWidget.add(items.createState().textArea(context,k.name,nameController));
+              }
+              break;
+            case 'Number':
+              {
+                final nameController = TextEditingController();
+                listWidget.add(items.createState().number(context,k.name,nameController));
+              }
+              break;
+            case 'Date':
+              {
+                listWidget.add(items.createState().date(context,k.name));
+              }
+              break;
+            case 'Table':
+              {
+                optionsElements = k.fieldOptions;
+                listWidget.add(items.createState().tab(optionsElements,context));
+              }
+              break;
+            case 'CanvanSignature':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            case 'Photo':
+              {
+                listWidget.add(items.createState().imagePhoto(context,k.name));
+              }
+              break;
+            case 'Image':
+              {
+                listWidget.add(items.createState().imageImage(context,k.name));
+              }
+              break;
+          //Desde aca para abajo
+            case 'Time':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            case 'DateTime':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            case 'ComboSearch':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            case 'Boolean':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            case 'CanvanImage':
+              {
+                listWidget.add(items.createState().loadingTask(k.fieldType));
+              }
+              break;
+            default:
+              {
+                listWidget.add(items.createState().label(k.fieldType));
+              }
+              break;
           }
-          inFieldModel.add(listFieldPrint);
         }
-      } else {}
-    }catch(e){}
+      }
+      inListWidget.add(listWidget);
+    }else{
+    }
   }
+
   @override
   void dispose() {
     _taskFormController.close();
     _taskTypeFormController.close();
     _formController.close();
-    saveFormController.close();
-    fieldModelController.close();
+    _saveFormController.close();
   }
 
   BlocTaskForm(context) {
--- lib/widgets/FormTaskNew.dart
+++ lib/widgets/FormTaskNew.dart
@@ -6,7 +6,6 @@ import 'package:joincompany/main.dart';
 import 'package:joincompany/models/FormModel.dart';
 import 'package:joincompany/models/FormsModel.dart';
 import 'package:joincompany/models/UserDataBase.dart';
-import 'package:joincompany/models/WidgetsList.dart';
 import 'package:joincompany/pages/BuscarRuta/BuscarDireccion.dart';
 import 'package:joincompany/services/FormService.dart';
 import 'package:http/http.dart' as http;
@@ -14,7 +13,7 @@ import 'package:joincompany/Sqlite/database_helper.dart';
 
 
 class FormTask extends StatefulWidget {
-  
+
 
   @override
   _FormTaskState createState() => new _FormTaskState();
@@ -25,7 +24,8 @@ class _FormTaskState extends State<FormTask> {
 
 
 //  List<Widget> listWidget = List<Widget>();
-
+  List<String> listElement = List<String>();
+  List<Widget> listWidgetMain = List<Widget>();
   bool changedView = false;
 
   UserDataBase userToken ;
@@ -34,21 +34,21 @@ class _FormTaskState extends State<FormTask> {
   String user;
   FormsModel formType;
 
-@override
-void initState(){
-  BuildOwner();
-  initFormType();
+  @override
+  void initState(){
+    BuildOwner();
+    initFormType();
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
 
-  final BlocTaskForm _bloc = new BlocTaskForm(context);
+    final BlocTaskForm _bloc = new BlocTaskForm(context);
     return new Scaffold(
-       appBar: AppBar(
-         elevation: 12,
-         backgroundColor: PrimaryColor,
-         leading:  IconButton(
+      appBar: AppBar(
+        elevation: 12,
+        backgroundColor: PrimaryColor,
+        /*leading:  IconButton(
            icon: Icon(Icons.arrow_back),
            tooltip: 'Guardar Tarea',
            iconSize: 35,
@@ -56,67 +56,64 @@ void initState(){
              StreamBuilder<String>(
                  stream: _bloc.outSaveForm,
                  builder: (context, snapshot) {
-
                  }
              );
              Navigator.pop(context);
              Navigator.pushReplacementNamed(context, '/vistap');
            },
+         ) ,*/
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete),
+            tooltip: 'Eliminar Cliente',
+            iconSize: 35,
+            onPressed: (){},
 
-         ) ,
-         actions: <Widget>[
-           IconButton(
-             icon: Icon(Icons.delete),
-             tooltip: 'Eliminar Cliente',
-             iconSize: 35,
-             onPressed: (){},
+          )
 
-           )
+        ],
+        title: Text('Agregar Tareas'),
+      ),
 
-         ],
-         title: Text('Agregar Tareas'),
-       ),
+      body:Stack(
+        children: <Widget>[
+          StreamBuilder<List<dynamic>>(
+              stream: _bloc.outListWidget,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                if (ConnectionState.active != null) {
+                  final data = snapshot.data;
+                  if(snapshot.hasData)
+                  {
+                    return  buildView(data);
+                  }
+                  else
+                  {
+                    return Center(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: Center(
+                              child: CircularProgressIndicator(
+                              ),
+                            ),
+                          ),
+                          Text('Seleccione un Formulario')
+                        ],
+                      ),
+                    );
+                  }
+                }else{
+                  CircularProgressIndicator(
 
-        body:Stack(
-          children: <Widget>[
-             StreamBuilder<List<dynamic>>(
-            stream: _bloc.outFieldModel,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-              if (ConnectionState.active != null) {
-                    final data = snapshot.data;
-                    if(snapshot.hasData)
-                    {
+                  );
 
-                      return   loadingData(data);
-                    }
-                   else
-                     {
-                       return Center(
-                         child: Column(
-                           children: <Widget>[
-                             Container(
-                               child: Center(
-                                 child: CircularProgressIndicator(
-                                 ),
-                               ),
-                             ),
-                             Text('Seleccione un Formulario')
-                           ],
-                         ),
-                       );
-                     }
-              }else{
-                CircularProgressIndicator(
-
-                );
+                }
 
               }
-
-            }
-        ),
-          ],
-        ),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: PrimaryColor,
         child: new Row(
@@ -136,34 +133,35 @@ void initState(){
                   showModalBottomSheet<String>(
                       context: context,
                       builder: (BuildContext context) {
-                              return new ListView.builder(
-                               itemCount: formType.data.length,//formType.data.length,
-                                itemBuilder: (BuildContext context, index){
-                                   return ListTile(
-                                     title: Text('${formType.data[index].name}'),
-                                     leading: Icon(Icons.label),
-                                     onTap: () async {
-                                      try{
-                                        var getFormResponse = await getForm(formType.data[index].id.toString(), customer, token);
-                                        FormModel form = FormModel.fromJson(getFormResponse.body);
-                                        _bloc.idFormType = formType.data[index].id.toString();
-                                        _bloc.customer = customer;
-                                        _bloc.token = token;
-                                        _bloc.form = form;
-                                        // getFormResponse.body.split(' ').forEach((word) => print(" " + word));
-                                        _bloc.updateListWidget(context);
+                        return new ListView.builder(
+                          itemCount: formType.data.length,//formType.data.length,
+                          itemBuilder: (BuildContext context, index){
+                            return ListTile(
+                              title: Text('${formType.data[index].name}'),
+                              leading: Icon(Icons.label),
+                              onTap: () async {
 
-                                        Navigator.pop(context);
-                                      }catch(e){}
+                                var getFormResponse = await getForm(formType.data[index].id.toString(), customer, token);
+                                FormModel form = FormModel.fromJson(getFormResponse.body);
+                                _bloc.idFormType = formType.data[index].id.toString();
+                                _bloc.customer = customer;
+                                _bloc.token = token;
+                                _bloc.form = form;
+                                // getFormResponse.body.split(' ').forEach((word) => print(" " + word));
+                                _bloc.updateListWidget(context);
 
-                                     },
+                                Navigator.pop(context);
 
-                                   );
 
-                                },
-                              );
-                            }
+
+                              },
+
+                            );
+
+                          },
                         );
+                      }
+                  );
                 }
             ),
 
@@ -173,143 +171,45 @@ void initState(){
     );
 
   }
-  Widget loadingData(data){
-    ListWidgets items = new ListWidgets();
-    List<Widget> listWidget = List<Widget>();
-      return  ListView.builder(
+  Widget buildView(data){
+
+    return  ListView.builder(
         itemCount: data.length,
         itemBuilder: (BuildContext context, int index) {
-          switch(data[index].fieldType){
+          return data[index];
+        }
+    ) ;
+  }
 
-            case 'Combo':
-              {
-                listWidget.add(
-                    items.createState().combo(data[index].fieldOptions, data[index].name));
-              }
-              break;
-            case 'Text':
-              {
-                final nameController = TextEditingController();
-                listWidget.add(items.createState().text(
-                    context, data[index].name, nameController));
-              }
-              break;
-            case 'Textarea':
-              {
-                final nameController = TextEditingController();
-                listWidget.add(items.createState().textArea(
-                    context, data[index].name, nameController));
-              }
-              break;
-            case 'Number':
-              {
-                final nameController = TextEditingController();
-                listWidget.add(items.createState().number(
-                    context, data[index].name, nameController));
-              }
-              break;
-            case 'Date':
-              {
-                listWidget.add(items.createState().date(context, data[index].name));
-              }
-              break;
-            case 'Table':
-              {
-                listWidget.add(
-                    items.createState().tab(data[index].fieldOptions, context));
-              }
-              break;
-            case 'CanvanSignature':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            case 'Photo':
-              {
-                listWidget.add(
-                    items.createState().imagePhoto(context, data[index].name));
-              }
-              break;
-            case 'Image':
-              {
-                listWidget.add(
-                    items.createState().imageImage(context, data[index].name));
-              }
-              break;
-          //Desde aca para abajo
-            case 'Time':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            case 'DateTime':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            case 'ComboSearch':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            case 'Boolean':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            case 'CanvanImage':
-              {
-                listWidget.add(items.createState().loadingTask(data[index].fieldType));
-              }
-              break;
-            default:
-              {
-                listWidget.add(items.createState().label(data[index].fieldType));
-              }
-              break;
-          }
-          return Column(
-            children: <Widget>[
-              listWidget[index],
-            ],
-          );
-    }
-
-    );
-
-}
-
-
-  
   getAll()async{
     FormsModel forms;
     FormsModel formType;
     await getElements();
     http.Response getAllFormsResponse = await getAllForms(customer , token);
-  try{
+    try{
 
-    if(getAllFormsResponse.statusCode == 200)
-    {
-    //  print(getAllFormsResponse.headers['content-type']);
-      forms = FormsModel.fromJson(getAllFormsResponse.body);
-      formType = forms;
+      if(getAllFormsResponse.statusCode == 200)
+      {
+        //  print(getAllFormsResponse.headers['content-type']);
+        forms = FormsModel.fromJson(getAllFormsResponse.body);
+        formType = forms;
+
+      }
+    }catch(e){
 
     }
-  }catch(e){
-
-  }
- return formType;
+    return formType;
   }
 
   initFormType()async{
-  formType = await getAll();
+    formType = await getAll();
   }
 
   getElements()async{
     userToken = await ClientDatabaseProvider.db.getCodeId('1');
-  token = userToken.token;
-  customer = userToken.company;
-  user = userToken.name;
+    token = userToken.token;
+    customer = userToken.company;
+    user = userToken.name;
 
   }
   void _showModalDateTimeAndDirections() {
@@ -344,5 +244,4 @@ void initState(){
   }
 
 }
-
 
