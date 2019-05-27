7c3ff1c5134eb971a55c86fa6e00749a1d5b9164
pc <jesus.cortez.1991@gmail.com>
2019-05-27 10:51:26 -0400

    eliminar form

--- lib/Menu/clientes.dart
+++ lib/Menu/clientes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:joincompany/Sqlite/database_helper.dart';
 import 'package:joincompany/blocs/blocCustomer.dart';
 import 'package:joincompany/main.dart';
 import 'package:joincompany/Menu//FormClients.dart';
@@ -7,7 +8,10 @@ import 'package:joincompany/Menu/configCli.dart';
 import 'package:joincompany/Menu/contactView.dart';
 import 'package:joincompany/models/CustomerModel.dart';
 import 'package:joincompany/models/CustomersModel.dart';
+import 'package:joincompany/models/UserDataBase.dart';
+import 'package:joincompany/models/UserModel.dart';
 import 'package:joincompany/models/WidgetsList.dart';
+import 'package:joincompany/services/UserService.dart';
 class Cliente extends StatefulWidget {
   @override
   _ClienteState createState() => _ClienteState();
@@ -21,6 +25,45 @@ class _ClienteState extends State<Cliente> {
   Widget _appBarTitle = new Text('Clientes');
   String textFilter='';
   final TextEditingController _filter = new TextEditingController();
+  String nameUser = '';
+  String emailUser = '';
+
+  @override
+  void initState() {
+    extraerUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      drawer: buildDrawer(),
+      appBar: AppBar(
+        title: _appBarTitle,
+        actions: <Widget>[
+          ls.createState().searchButtonAppbar(_searchIcon, _searchPressed, 'Eliminar Tarea', 30),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          listViewCustomers(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        elevation: 12,
+        backgroundColor: PrimaryColor,
+        tooltip: 'Agregar Tarea',
+        onPressed: (){
+          Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (BuildContext context) => new  FormClient(null)));
+        },
+
+      ),
+    );
+  }
 
   Widget clientCard(String titleCli, String subtitleCli, int idCli) {//TODO: change String for Client
     String title = titleCli;
@@ -58,6 +101,7 @@ class _ClienteState extends State<Cliente> {
     );
   }
 
+  bool drawerCustomer = true;
   Drawer buildDrawer() {
     return Drawer(
       elevation: 12,
@@ -66,47 +110,53 @@ class _ClienteState extends State<Cliente> {
           new UserAccountsDrawerHeader(
             decoration: new BoxDecoration(color: SecondaryColor,
             ),
-            accountName: new Text('Nombre de la empresa:',
+            accountName: new Text(nameUser,
               style: TextStyle(
                 color: Colors.black,
-                fontSize: 18,
+                fontSize: 16,
               ),
             ),
-            accountEmail : Text('Nombre de Usuario',
+            accountEmail : Text(emailUser,
               style: TextStyle(
                 color: Colors.black,
-                fontSize: 17,
-              ),),),
-          new ListTile(
-            trailing: new Icon(Icons.assignment),
-            title: new Text('Tareas'),
-
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.pushReplacementNamed(context, '/vistap');
-            },
+                fontSize: 15,
+              ),
+            ),
           ),
-          new ListTile(
-            title: new Text("Clientes"),
-            trailing: new Icon(Icons.business),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (BuildContext context) => new  Cliente()));
-            },
+          Container(
+              child: ListTile(
+                trailing: new Icon(Icons.assignment),
+                title: new Text('Tareas'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              )
           ),
-          new ListTile(
-            title: new Text("Contactos"),
-            trailing: new Icon(Icons.contacts),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (BuildContext context) => new  ContactView()));
-            },
+          Container(
+            color: drawerCustomer ? Colors.grey[200] :  null,
+            child: ListTile(
+              title: new Text("Clientes"),
+              trailing: new Icon(Icons.business),
+              onTap: () {
+                Navigator.pop(context);
+//                Navigator.pushNamed(context, '/cliente');
+//              Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new  Cliente()));
+              },
+            ),
+          ),
+          Container(
+            child: new ListTile(
+              title: new Text("Contactos"),
+              trailing: new Icon(Icons.contacts),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (BuildContext context) => new  ContactView()));
+              },
+            ),
           ),
           /*new ListTile(
             title: new Text("Negocios"),
@@ -118,21 +168,36 @@ class _ClienteState extends State<Cliente> {
           Divider(
             height: 30.0,
           ),
-          new ListTile(
-            title: new Text("Configuracion"),
-            trailing: new Icon(Icons.filter_vintage),
-            onTap: () {
-              // Navigator.pushReplacementNamed(context, "/intro");
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (BuildContext context) => new  ConfigCli()));
-            },
+          Container(
+            child: new ListTile(
+              title: new Text("Configuración"),
+              trailing: new Icon(Icons.filter_vintage),
+              onTap: () {
+                // Navigator.pushReplacementNamed(context, "/intro");
+                Navigator.of(context).pop();
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new  ConfigCli()));
+              },
+            ),
           ),
         ],
       ),
     );
+  }
+
+
+  extraerUser() async {
+    UserDataBase userAct = await ClientDatabaseProvider.db.getCodeId('1');
+    var getUserResponse = await getUser(userAct.company, userAct.token);
+    UserModel user = UserModel.fromJson(getUserResponse.body);
+
+    setState(() {
+      nameUser = user.name;
+      emailUser = user.email;
+    });
   }
 
   void _searchPressed() {
@@ -160,37 +225,6 @@ class _ClienteState extends State<Cliente> {
         _filter.clear();
       }
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      //drawer: buildDrawer(),
-      appBar: AppBar(
-        title: _appBarTitle,
-        actions: <Widget>[
-          ls.createState().searchButtonAppbar(_searchIcon, _searchPressed, 'Eliminar Tarea', 30),
-        ],
-      ),
-      body: Stack(
-        children: <Widget>[
-          listViewCustomers(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        elevation: 12,
-        backgroundColor: PrimaryColor,
-        tooltip: 'Agregar Tarea',
-        onPressed: (){
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (BuildContext context) => new  FormClient(null)));
-        },
-
-      ),
-    );
   }
 
   listViewCustomers(){
--- lib/Menu/contactView.dart
+++ lib/Menu/contactView.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:joincompany/Menu/addContact.dart';
+import 'package:joincompany/Sqlite/database_helper.dart';
 import 'package:joincompany/models/CustomersModel.dart';
+import 'package:joincompany/models/UserDataBase.dart';
+import 'package:joincompany/models/UserModel.dart';
+import 'package:joincompany/services/UserService.dart';
+
+import '../main.dart';
+import 'configCli.dart';
 class ContactView extends StatefulWidget {
   @override
   _ContactViewState createState() => _ContactViewState();
 }
 
 class _ContactViewState extends State<ContactView> {
+
+  String nameUser = '';
+  String emailUser = '';
+
+  @override
+  void initState() {
+    extraerUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: buildDrawer(),
+      appBar: AppBar(
+        title: Text(""),
+      ),
+//      body: FutureBuilder<List<String>>(//TODO: change String for Contacts
+//        future: null, //TODO: getAllContacts()
+//        builder: (BuildContext contex, AsyncSnapshot<List<String>> snapshot){//TODO: change String for Contacts
+//          if (snapshot.hasData) {
+//            return ListView.builder(
+//                itemCount: snapshot.data.length,
+//                itemBuilder: (BuildContext contex, int index){
+//                  String item = snapshot.data[index]; //TODO: change String for Contacts
+//                  return contactCard(item);
+//                }
+//            );
+//          }else{
+//            return Center(child: CircularProgressIndicator());
+//          }
+//        },
+//      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            contactCard("Contacto 1"),
+            contactCard("Contacto 2"),
+            contactCard("Contacto 3"),
+            contactCard("Contacto 4"),
+            contactCard("Contacto 5"),
+            contactCard("Contacto 6"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed:(){
+          Navigator.of(context).pop();
+          Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (BuildContext context) => new  AddContact()));
+        },
+      ),
+    );
+  }
+
+
   Future<List<CustomersModel>> data(){ //TODO
     //CustomerService.getAllCustomers();
     return null;
@@ -52,50 +118,99 @@ class _ContactViewState extends State<ContactView> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
-      ),
-//      body: FutureBuilder<List<String>>(//TODO: change String for Contacts
-//        future: null, //TODO: getAllContacts()
-//        builder: (BuildContext contex, AsyncSnapshot<List<String>> snapshot){//TODO: change String for Contacts
-//          if (snapshot.hasData) {
-//            return ListView.builder(
-//                itemCount: snapshot.data.length,
-//                itemBuilder: (BuildContext contex, int index){
-//                  String item = snapshot.data[index]; //TODO: change String for Contacts
-//                  return contactCard(item);
-//                }
-//            );
-//          }else{
-//            return Center(child: CircularProgressIndicator());
-//          }
-//        },
-//      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            contactCard("Contacto 1"),
-            contactCard("Contacto 2"),
-            contactCard("Contacto 3"),
-            contactCard("Contacto 4"),
-            contactCard("Contacto 5"),
-            contactCard("Contacto 6"),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed:(){
-          Navigator.of(context).pop();
-          Navigator.push(
-              context,
-              new MaterialPageRoute(
-                  builder: (BuildContext context) => new  AddContact()));
-        },
+
+  bool drawerCustomer = true;
+  Drawer buildDrawer() {
+    return Drawer(
+      elevation: 12,
+      child: new ListView(
+        children: <Widget>[
+          new UserAccountsDrawerHeader(
+            decoration: new BoxDecoration(color: SecondaryColor,
+            ),
+            accountName: new Text(nameUser,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+            accountEmail : Text(emailUser,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+              child: ListTile(
+                trailing: new Icon(Icons.assignment),
+                title: new Text('Tareas'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              )
+          ),
+          Container(
+            child: ListTile(
+              title: new Text("Clientes"),
+              trailing: new Icon(Icons.business),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/cliente');
+              },
+            ),
+          ),
+          Container(
+            color: drawerCustomer ? Colors.grey[200] :  null,
+            child: new ListTile(
+              title: new Text("Contactos"),
+              trailing: new Icon(Icons.contacts),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          /*new ListTile(
+            title: new Text("Negocios"),
+            trailing: new Icon(Icons.poll),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),*/
+          Divider(
+            height: 30.0,
+          ),
+          Container(
+            child: new ListTile(
+              title: new Text("Configuración"),
+              trailing: new Icon(Icons.filter_vintage),
+              onTap: () {
+                // Navigator.pushReplacementNamed(context, "/intro");
+                Navigator.of(context).pop();
+                Navigator.pop(context);
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new  ConfigCli()));
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+
+  extraerUser() async {
+    UserDataBase userAct = await ClientDatabaseProvider.db.getCodeId('1');
+    var getUserResponse = await getUser(userAct.company, userAct.token);
+    UserModel user = UserModel.fromJson(getUserResponse.body);
+
+    setState(() {
+      nameUser = user.name;
+      emailUser = user.email;
+    });
   }
 }
