ae08646e68193b1f8d29f9a17e7bf12d941682d0
pc <jesus.cortez.1991@gmail.com>
2019-05-29 16:22:28 -0400

    Merge branch 'luis_view' of https://github.com/HectorSoftware/joincompany into JesusHoy
    # Conflicts:
    #	lib/pages/LoginPage.dart

--- lib/pages/home/TaskHomeTask.dart
+++ lib/pages/home/TaskHomeTask.dart
@@ -335,7 +335,7 @@ class _MytaskPageTaskState extends State<taskHomeTask> {
                     child: IconButton(
                         icon: Icon(Icons.delete),
                         onPressed: () {
-                          deleteCustomer(listTask.id.toString());
+                          deleteCustomer(listTask.id.toString(),index);
                         }
                     ),
                   ),
@@ -348,10 +348,11 @@ class _MytaskPageTaskState extends State<taskHomeTask> {
   }
 
 
-  Future <bool> deleteCustomer(String taskID) async {
+  deleteCustomer(String taskID, int index) async {
     var deleteTaskResponse = await deleteTask(taskID,UserActiv.company,UserActiv.token);
-    print(deleteTaskResponse.statusCode);
-    return true;
+    if(deleteTaskResponse.statusCode == 200){
+      listTaskModellocal.removeAt(index);
+    }
   }
 
   actualizarusuario() async{
