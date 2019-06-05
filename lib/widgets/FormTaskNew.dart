aac1d481fa3b55dbb5563eeb6bd44bdb756811cb
SanvicenteCesar <cesarsvp17@gmail.com>
2019-06-05 10:48:36 -0400

    combo finalizado Realizando Cambios en TIME

--- lib/widgets/FormTaskNew.dart
+++ lib/widgets/FormTaskNew.dart
@@ -614,7 +614,7 @@ class _FormTaskState extends State<FormTask> {
                     Padding(
                       padding: const EdgeInsets.only(left: 10),
                       child: RaisedButton(
-                        child: dataInfo[listFieldsModels[index].id.toString()] == null ? Text('${dataInfo[listFieldsModels[index].id]}') : Text('Sin Asignar'),
+                        child: dataInfo[listFieldsModels[index].id.toString()] == null ? Text('${dataInfo[listFieldsModels[index].id.toString()]}') : Text('Sin Asignar'),
                         onPressed: (){selectTime(context);
                         saveData(_time.format(context).toString(),  listFieldsModels[index].id.toString()) ;
                         print(dataInfo[listFieldsModels[index].id]);
