e0c84eace7aa31c7fea023f054c54fdeb38b750d
Stalin Sánchez <cejonio@gmail.com>
2019-06-07 14:24:10 -0400

    Se agregó ON UPDATE CASCADE a las referencias en la BD

--- lib/pages/BuscarRuta/searchAddressWithClient.dart
+++ lib/pages/BuscarRuta/searchAddressWithClient.dart
@@ -109,7 +109,7 @@ class _SearchAddressState extends State<searchAddressWithClient> {
 
   void _getUserLocation() async{
     Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
+    // List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
 
     setState(() {
       _initialPosition = LatLng(position.latitude, position.longitude);
